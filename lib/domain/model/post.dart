e5d491f2f5d50dddefb817c0c4eaac782a716e05
sinbee0402 <osb3476@gmail.com>
2023-07-28 15:54:15 +0900

    Feat: model 작성

---
+++ lib/domain/model/post.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'post.freezed.dart';
+
+part 'post.g.dart';
+
+@freezed
+class Post with _$Post {
+  const factory Post({
+    required String emoji,
+    required DateTime date,
+    required String title,
+    required String content,
+  }) = _Post;
+
+  factory Post.fromJson(Map<String, Object?> json) => _$PostFromJson(json);
+}
