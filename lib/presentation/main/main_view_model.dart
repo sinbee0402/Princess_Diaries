fdda7a529506aab3b9d4817f90f1910d8cccabad
sinbee0402 <osb3476@gmail.com>
2023-08-09 15:42:16 +0900

    Feat: main view model 수정

--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -35,6 +35,7 @@ class MainViewModel with ChangeNotifier {
     if (id == null) {
       await useCases.addPost(
         Post(
+            id: id,
             emoji:
                 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/220px-Noto_Emoji_KitKat_263a.svg.png',
             date: DateTime.now(),
