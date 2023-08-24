ce5890e141d068859a4ec899ad4f04c9d222281b
sinbee0402 <osb3476@gmail.com>
2023-08-24 14:40:33 +0900

    feat: main ui_event, view_model 수정

--- lib/presentation/main/main_ui_event.dart
+++ lib/presentation/main/main_ui_event.dart
@@ -1,16 +1,10 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
-import 'package:princess_diaries/domain/model/post.dart';
 
 part 'main_ui_event.freezed.dart';
 
 @freezed
 sealed class MainUiEvent<T> with _$MainUiEvent<T> {
   const factory MainUiEvent.loadPosts() = LoadPosts;
-  const factory MainUiEvent.savePost(
-    int? id,
-    String title,
-    String content,
-  ) = SavePost;
-  const factory MainUiEvent.deletePost(Post post) = DeletePost;
+  //const factory MainUiEvent.deletePost(Post post) = DeletePost;
 }
--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -1,64 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:princess_diaries/domain/model/post.dart';
-import 'package:princess_diaries/domain/use_case/use_cases.dart';
+import 'package:princess_diaries/domain/repository/post_repository.dart';
 import 'package:princess_diaries/presentation/main/main_state.dart';
 import 'package:princess_diaries/presentation/main/main_ui_event.dart';
 
 @injectable
 class MainViewModel with ChangeNotifier {
-  final UseCases useCases;
+  final PostRepository _repository;
 
   MainState _state = const MainState();
   MainState get state => _state;
 
-  MainViewModel(this.useCases);
+  MainViewModel(this._repository);
 
   void onEvent(MainUiEvent event) {
     switch (event) {
       case LoadPosts():
         _loadPosts;
-      case SavePost(:final id, :final title, :final content):
-        _savePost;
-      case DeletePost():
-        _deletePost;
+      // case DeletePost():
+      //   _deletePost;
     }
   }
 
   Future<void> _loadPosts() async {
-    List<Post> posts = await useCases.getPosts();
+    List<Post> posts = await _repository.getPosts();
     _state = state.copyWith(
       posts: posts,
     );
     notifyListeners();
   }
 
-  Future<void> _savePost(int? id, String title, String content) async {
-    if (id == null) {
-      await useCases.addPost(
-        Post(
-            id: id,
-            emoji:
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/220px-Noto_Emoji_KitKat_263a.svg.png',
-            date: DateTime.now(),
-            title: title,
-            content: content),
-      );
-    } else {
-      await useCases.updatePost(
-        Post(
-          emoji:
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/220px-Noto_Emoji_KitKat_263a.svg.png',
-          date: DateTime.now(),
-          title: title,
-          content: content,
-        ),
-      );
-    }
-  }
-
-  Future<void> _deletePost(Post post) async {
-    await useCases.deletePost(post);
-    await _loadPosts();
-  }
+  // Future<void> _deletePost(Post post) async {
+  //   await useCases.deletePost(post);
+  //   await _loadPosts();
+  // }
 }
