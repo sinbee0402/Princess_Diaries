078c2dc861ab4cdfbf925b1f3204fc25812161f7
sinbee0402 <osb3476@gmail.com>
2023-09-01 18:03:58 +0900

    fix: DB Table에 데이터가 insert 되지않던 bug 해결

--- lib/presentation/add_edit_post/add_edit_post_view_model.dart
+++ lib/presentation/add_edit_post/add_edit_post_view_model.dart
@@ -5,22 +5,18 @@ import 'package:injectable/injectable.dart';
 import 'package:princess_diaries/domain/model/post.dart';
 import 'package:princess_diaries/domain/repository/post_repository.dart';
 import 'package:princess_diaries/presentation/add_edit_post/add_edit_post_event.dart';
-import 'package:princess_diaries/presentation/add_edit_post/add_edit_post_ui_event.dart';
 
 @injectable
 class AddEditPostViewModel with ChangeNotifier {
   final PostRepository _repository;
-
-  final _eventController = StreamController<AddEditPostUiEvent>.broadcast();
-  Stream<AddEditPostUiEvent> get eventStream => _eventController.stream;
 
   AddEditPostViewModel(this._repository);
 
   void onEvent(AddEditPostEvent event) {
     switch (event) {
       //case ChangeBase(:final url):
-      case SavePost():
-        _savePost;
+      case SavePost(:final id, :final emoji, :final content):
+        _savePost(id, emoji, content);
     }
   }
 
