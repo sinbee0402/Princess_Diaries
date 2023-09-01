9dad1a0565b9b744bcb82b88060000f2059f2974
sinbee0402 <osb3476@gmail.com>
2023-09-01 17:42:22 +0900

    chore: add_edit_post_view_model 원상복구

--- lib/presentation/add_edit_post/add_edit_post_view_model.dart
+++ lib/presentation/add_edit_post/add_edit_post_view_model.dart
@@ -1,19 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:princess_diaries/domain/model/post.dart';
-import 'package:princess_diaries/domain/use_case/use_cases.dart';
+import 'package:princess_diaries/domain/repository/post_repository.dart';
 import 'package:princess_diaries/presentation/add_edit_post/add_edit_post_event.dart';
+import 'package:princess_diaries/presentation/add_edit_post/add_edit_post_ui_event.dart';
 
 @injectable
 class AddEditPostViewModel with ChangeNotifier {
-  final UseCases _useCases;
+  final PostRepository _repository;
 
-  AddEditPostViewModel(this._useCases);
+  final _eventController = StreamController<AddEditPostUiEvent>.broadcast();
+  Stream<AddEditPostUiEvent> get eventStream => _eventController.stream;
+
+  AddEditPostViewModel(this._repository);
 
   void onEvent(AddEditPostEvent event) {
     switch (event) {
       //case ChangeBase(:final url):
-
       case SavePost():
         _savePost;
     }
@@ -21,7 +26,7 @@ class AddEditPostViewModel with ChangeNotifier {
 
   Future<void> _savePost(int? id, String emoji, String content) async {
     if (id == null) {
-      await _useCases.addPost(
+      await _repository.insertPost(
         Post(
           emoji: emoji,
           date: DateTime.now().millisecondsSinceEpoch,
@@ -29,7 +34,7 @@ class AddEditPostViewModel with ChangeNotifier {
         ),
       );
     } else {
-      await _useCases.addPost(
+      await _repository.updatePost(
         Post(
           id: id,
           emoji: emoji,
