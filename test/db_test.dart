a8ef60eb8842ac8da910a608916c2f73826a427c
sinbee0402 <osb3476@gmail.com>
2023-09-13 16:12:22 +0900

    feat: db 코드 수정 후 test 코드가 잘 작동되는 것을 확인

--- test/db_test.dart
+++ test/db_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
 import 'package:princess_diaries/data/data_source/post_db.dart';
 import 'package:princess_diaries/domain/model/post.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
@@ -7,33 +8,37 @@ void main() {
   test('db test', () async {
     final db = await databaseFactoryFfi.openDatabase(inMemoryDatabasePath);
     await db.execute(
-        'CREATE TABLE post (id INTEGER PRIMARY KEY AUTOINCREMENT, emoji TEXT, date INTEGER, content TEXT)');
+      'CREATE TABLE post (id INTEGER PRIMARY KEY AUTOINCREMENT, emoji TEXT, content TEXT, date TEXT, yearMonth INTEGER)',
+    );
     // table 생성 테스트
-    expect(db.isOpen, true);
+    expect(db.isOpen, true); // pass
 
     final postDb = PostDb(db);
-    await postDb.insertPost(Post(
-      id: 1,
-      emoji: 'test1',
-      date: DateTime.now().millisecondsSinceEpoch,
-      content: 'test1',
-    ));
+    await postDb.insertPost(
+      Post(
+        id: 0,
+        emoji: 'test',
+        content: 'test1',
+        date: DateFormat('yyyy-MM-dd,H:m').format(DateTime.now()),
+        yearMonth: int.parse(DateFormat('yyyyMM').format(DateTime.now())),
+      ),
+    );
     // insert 테스트
-    expect((await postDb.getPosts()).length, 1);
+    expect((await postDb.getPosts(202309)).length, 1);
 
-    Post post = (await postDb.getPostById(1))!;
+    Post post = (await postDb.getPostById(0))!;
     // getPostById 테스트
-    expect(post.id, 1);
+    expect(post.id, 0);
 
     await postDb.updateNote(post.copyWith(
       content: 'change',
     ));
-    post = (await postDb.getPostById(1))!;
+    post = (await postDb.getPostById(0))!;
     // update 테스트
     expect(post.content, 'change');
 
     await postDb.deletePost(post);
-    expect((await postDb.getPosts()).length, 0);
+    expect((await postDb.getPosts(202309)).length, 0);
 
     await db.close();
   });
