ae2798fc89e5579cd1d20b217db706be122030b7
sinbee0402 <osb3476@gmail.com>
2023-09-20 01:08:17 +0900

    feat: emoji_popup UI 수정

--- lib/presentation/components/emoji_popup.dart
+++ lib/presentation/components/emoji_popup.dart
@@ -17,82 +17,114 @@ class _EmojiPopupState extends State<EmojiPopup> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      backgroundColor: Colors.white.withOpacity(0.7),
-      title: const Text(
-        '감정을 선택해주세요!',
-        style: TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.w500,
-        ),
+      backgroundColor: Colors.white,
+      title: const Column(
+        children: [
+          Text(
+            '오늘 하루는 어땠나요?',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: Color(0xFFED0F69),
+            ),
+          ),
+          SizedBox(height: 16)
+        ],
       ),
       content: Wrap(
+        alignment: WrapAlignment.center,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          Column(
             children: [
-              InkWell(
-                onTap: () {
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      context.pop('${widget.emojiTheme}/1.png');
+                    },
+                    child: Image.asset(
+                      '${widget.emojiTheme}/1.png',
+                      fit: BoxFit.fill,
+                      width: 60,
+                    ),
+                  ),
+                  const SizedBox(width: 30),
+                  InkWell(
+                    onTap: () {
+                      context.pop('${widget.emojiTheme}/2.png');
+                    },
+                    child: Image.asset(
+                      '${widget.emojiTheme}/2.png',
+                      fit: BoxFit.fill,
+                      width: 60,
+                    ),
+                  ),
+                  const SizedBox(width: 30),
+                  InkWell(
+                    onTap: () {
+                      context.pop('${widget.emojiTheme}/3.png');
+                    },
+                    child: Image.asset(
+                      '${widget.emojiTheme}/3.png',
+                      fit: BoxFit.fill,
+                      width: 60,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      context.pop('${widget.emojiTheme}/4.png');
+                    },
+                    child: Image.asset(
+                      '${widget.emojiTheme}/4.png',
+                      fit: BoxFit.fill,
+                      width: 60,
+                    ),
+                  ),
+                  const SizedBox(width: 30),
+                  InkWell(
+                    onTap: () {
+                      context.pop('${widget.emojiTheme}/5.png');
+                    },
+                    child: Image.asset(
+                      '${widget.emojiTheme}/5.png',
+                      fit: BoxFit.fill,
+                      width: 60,
+                    ),
+                  ),
+                  const SizedBox(width: 30),
+                  InkWell(
+                    onTap: () {
+                      context.pop('${widget.emojiTheme}/6.png');
+                    },
+                    child: Image.asset(
+                      '${widget.emojiTheme}/6.png',
+                      fit: BoxFit.fill,
+                      width: 60,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
                   context.pop('assets/icon-question-mark.png');
                 },
-                child: Image.asset(
-                  'assets/icon-question-mark.png',
-                  fit: BoxFit.cover,
-                  width: 80,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF7B7D3),
                 ),
-              ),
-              InkWell(
-                onTap: () {
-                  context.pop('${widget.emojiTheme}/1.png');
-                },
-                child: Image.asset(
-                  '${widget.emojiTheme}/1.png',
-                  fit: BoxFit.fill,
-                  width: 80,
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  context.pop('${widget.emojiTheme}/2.png');
-                },
-                child: Image.asset(
-                  '${widget.emojiTheme}/2.png',
-                  fit: BoxFit.fill,
-                  width: 80,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              InkWell(
-                onTap: () {
-                  context.pop('${widget.emojiTheme}/3.png');
-                },
-                child: Image.asset(
-                  '${widget.emojiTheme}/3.png',
-                  fit: BoxFit.fill,
-                  width: 80,
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  context.pop('${widget.emojiTheme}/4.png');
-                },
-                child: Image.asset(
-                  '${widget.emojiTheme}/4.png',
-                  fit: BoxFit.fill,
-                  width: 80,
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  context.pop('${widget.emojiTheme}/5.png');
-                },
-                child: Image.asset(
-                  '${widget.emojiTheme}/5.png',
-                  fit: BoxFit.fill,
-                  width: 80,
+                child: const Text(
+                  '선택하지 않을래요',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
             ],
